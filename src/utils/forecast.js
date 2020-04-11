6135d432c5ccb7fc62eb985884bbd622ab7fa067
Ellworth Acquaye <ellworth.acquaye@gmail.com>
2020-04-11 11:43:08 +0000

    Initial Commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,16 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url =  'http://api.openweathermap.org/data/2.5/weather?lat='+ latitude +'&lon='+ longitude +'&appid=b10c74b18fc7ed2c7111ec0296730fe9'
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            callback(undefined, body.weather[0].description + ' It is currently ' + body.main.temp + ' degrees out. With humidity levels at ' +body.main.humidity)
+        }
+    })
+}
+
+module.exports = forecast
